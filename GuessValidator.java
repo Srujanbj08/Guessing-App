7a11dc67574453bccd997513f0a80c26bdd2c6dd
Srujanbj08 <srujanbj.ai22@bmsce.ac.in>
2026-02-23 14:09:14 +0530

    added userguesssubmission code

---
+++ GuessValidator.java
@@ -0,0 +1,11 @@
+public class GuessValidator {
+    public static String validateGuess(int guess, int target) {
+
+        if (guess == target) {
+            return "CORRECT";
+        } else if (guess < target) {
+            return "LOW";
+        }
+        return "HIGH";
+    }
+}
--- GuessingAPP.java
+++ GuessingAPP.java
@@ -1,7 +1,30 @@
-public class GuessingApp {
+import java.util.*;
+public class GuessingAPP {
+
     public static void main(String[] args) {
-        System.out.println("Welcome to the Guessing App!");
-        GameConfig gameConfig = new GameConfig();
-        gameConfig.showRules();
+
+        System.out.println("Welcome to the Guessing App");
+
+        GameConfig config = new GameConfig();
+        config.showRules();
+
+        Scanner scanner = new Scanner(System.in);
+        int attempts = 0;
+
+while (attempts < config.getMaxAttempts()) {
+
+    System.out.print("Enter your guess: ");
+    int guess = scanner.nextInt();
+    attempts++;
+
+    String result = GuessValidator.validateGuess(guess, config.getTargetNumber());
+
+    System.out.println(result);
+
+    
+    if ("CORRECT".equals(result)) {
+        break;
     }
+}
+}
 }
